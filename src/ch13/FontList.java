4b0cb0f22639083f5160f4f75e9e6548821df7b3
jsw <jsw@DESKTOP-RB0B1GQ>
2020-11-19 09:06:07 +0900

    study

---
+++ src/ch13/FontList.java
@@ -0,0 +1,16 @@
+package ch13;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+
+public class FontList {
+	public static void main(String[] args) {
+		GraphicsEnvironment ge = null;
+		ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		Font[] fonts = ge.getAllFonts();
+		
+		for (int i = 0; i < fonts.length; i++) {
+			System.out.println(fonts[i].getFontName());
+		}
+	}
+}
