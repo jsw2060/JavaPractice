c37df6249e2f8731fedf30e48665c6450f085996
jsw <jsw@DESKTOP-RB0B1GQ>
2020-11-26 06:39:15 +0900

    study

---
+++ src/ch13/MouseEventTest.java
@@ -0,0 +1,44 @@
+package ch13;
+
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MouseEventTest extends Frame{
+	Label location;
+	
+	public MouseEventTest(String title) {
+		super(title);	// Frame(String title)을 호출한다.
+		location = new Label("Mouse Pointer Location : ");
+		location.setSize(195, 15);
+		location.setLocation(5, 30);
+		location.setBackground(Color.yellow); 	// Label의 배경색을 노란색으로 한다.
+		add(location);
+		
+		// EventHandler의 인스턴스를 Frame의 Listener로 등록한다.
+		addMouseMotionListener(new EventHandler());
+		
+		setSize(300, 200);
+		setLayout(null);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		MouseEventTest mainWin = new MouseEventTest("MouseEventTest");
+	}	// main메서드의 끝
+	
+	class EventHandler implements MouseMotionListener {
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			location.setText("Mouse Pointer Location : (" + e.getX() + "," + e.getY() + ")");
+		}
+	}	// EventHandler클래스의 끝
+}
---
+++ src/ch13/TextComponentEventTest.java
@@ -0,0 +1,41 @@
+package ch13;
+
+import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextComponentEventTest extends Frame{
+	TextField tf;
+	TextArea ta;
+	
+	public TextComponentEventTest(String title) {
+		super(title);	// Frame(String title)을 호출한다.
+		
+		tf = new TextField();
+		ta = new TextArea();
+		add(ta, "Center");
+		add(tf, "South");
+		
+		tf.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TestField에서 Enter를 치면, tf에 입력된 text를 TextArea에 추가한다.
+				ta.append(tf.getText() + "\n");
+				tf.setText(""); 	// tf의 text를 지운다.
+				tf.requestFocus();
+			}
+		});
+		
+		ta.setEditable(false);	// TextArea의 text를 편집하지 못하게 한다.
+		setSize(300, 200);
+		setVisible(true);
+		tf.requestFocus(); 		// focus가 TextField에 위치하도록 한다.
+	}
+	
+	public static void main(String[] args) {
+		TextComponentEventTest mainWin = new TextComponentEventTest("TextComponentEventTest");
+	}	// main메서드의 끝
+}
